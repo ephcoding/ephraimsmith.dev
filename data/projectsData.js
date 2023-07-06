1f80eb1b8660fc4ea1d71f4b82260a71a7a09e88
Eph <ephraim@modevx.com>
2023-07-06 13:51:59 -0500

    remove rovercam project for now

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,5 @@
 const projectsData = [
   {
-    title: 'RoverCam',
-    description: `Explore the Red Planet through photos taken by the Mars Rovers, Curiosity, Opportunity, Perseverance, and Spirit.`,
-    imgSrc: '/static/images/project-rovercam.png',
-    href: '/blog/rovercam',
-  },
-  {
     title: 'Tornado Warned',
     description: `Tornado Warned is a web app that provides real-time alerts and visualizations for tornadic weather events.`,
     imgSrc: '/static/images/project-tornado-warned.png',
