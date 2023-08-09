72d235c6c85e9b44a234bd7811a198f06c698e03
Eph <ephraim@modevx.com>
2023-08-09 11:44:16 -0500

    change Tornado Warned project url to live site

--- data/projectsData.js
+++ data/projectsData.js
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'Tornado Warned',
     description: `Tornado Warned is a web app that provides real-time alerts and visualizations for tornadic weather events.`,
     imgSrc: '/static/images/project-tornado-warned.png',
-    href: '/blog/tornado-warned',
+    href: 'https://tornadowarned.com',
   },
 ]
 
